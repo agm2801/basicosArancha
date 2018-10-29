5dbb054cc0257d302c1259a2c13bb2c989a8768b
Arancha <Arancha@Arancha-PC>
2018-10-29 19:03:45 +0100

    añadido logicos

--- src/basicosarancha/BasicosArancha.java
+++ src/basicosarancha/BasicosArancha.java
@@ -12,10 +12,38 @@ package basicosarancha;
 public class BasicosArancha {
 
     /**
+     * Método para mostrar ejemplos de tipos logicos.
+     */
+    public static void logicos() {
+
+        //Estos son los tipos básicos
+        boolean t, f;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        t = true;
+        f = false;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        if ( t == f){
+            System.out.println("t y f son iguales en operador ==");
+        } else{
+            System.out.println("t y f son distintos en operador ==");
+        }
+        
+        if (t != f){
+            System.out.println("t y f son distintos en operador !=");
+        } else{
+            System.out.println("t y f son iguales en operador !=");
+        }
+    }
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        logicos();
     }
     
 }
