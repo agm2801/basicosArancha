fe5e4dad0c932b0f7d66b775056224bbb5200831
Arancha <Arancha@Arancha-PC>
2018-10-29 18:52:22 +0100

    añadido cadenas

--- src/basicosarancha/BasicosArancha.java
+++ src/basicosarancha/BasicosArancha.java
@@ -12,10 +12,27 @@ package basicosarancha;
 public class BasicosArancha {
 
     /**
+     * Método para mostrar ejemplos de tipos cadenas.
+     */
+    public static void cadenas() {
+
+        //Estos son los tipos básicos
+        String cadena;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        cadena = "cadena de String";
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println("Valor cadena: " + cadena);
+        System.out.println("Valor cadena + cadena: " + (cadena+cadena));
+    }
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        cadenas();
     }
     
 }
